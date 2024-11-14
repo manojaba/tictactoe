d525cc971eced0480837a3502aafc4b1948da80c
Manoj Baskar <147849968+manojaba@users.noreply.github.com>
2024-11-14 15:55:46 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export function Square() {
+
+  let [value, setvalue] = useState(null);
+
+  function handleClick() {
+    setvalue('x');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button style={{ height: 40, width: 40 }} onClick={handleClick}>{value}</button>
+    </>
+  )
 }
 
-export default App;
+
+
+export default function Board() {
+  return (
+    <>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+
+  )
+}
