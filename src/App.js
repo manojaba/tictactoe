fb80041d9200cde30f515ab3c321f0c3fc21d33c
Manoj Baskar <147849968+manojaba@users.noreply.github.com>
2024-11-14 16:46:41 +0530

    state lifteed up and props sent down to children

--- src/App.js
+++ src/App.js
@@ -1,39 +1,49 @@
 import { useState } from "react";
 
-export function Square() {
+export function Square({ value, onSquareClick }) {
 
-  let [value, setvalue] = useState(null);
 
-  function handleClick() {
-    setvalue('x');
-  }
+
   return (
     <>
-      <button style={{ height: 40, width: 40 }} onClick={handleClick}>{value}</button>
+      <button style={{ height: 40, width: 40 }} onClick={onSquareClick} >{value}</button>
     </>
   )
-}
+};
 
 
 
 export default function Board() {
+
+  let [squares, setSquares] = useState(Array(9).fill(null));
+
+
+
+  function handleClick(i) {
+    let nextSquares = squares.slice();
+    nextSquares[i] = 'x';
+
+    setSquares(nextSquares);
+
+
+  }
   return (
     <>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
 
       <div>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
     </>
 
